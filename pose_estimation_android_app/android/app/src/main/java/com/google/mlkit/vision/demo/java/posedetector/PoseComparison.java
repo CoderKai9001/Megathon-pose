560f8bc9c57b2c9077b73a4551f3be18c26b2eea
Chandramouli Vadali <147921963+CoderKai9001@users.noreply.github.com>
2024-10-27 12:02:57 +0530

    Update PoseComparison.java fix greens

--- pose_estimation_android_app/android/app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/PoseComparison.java
+++ pose_estimation_android_app/android/app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/PoseComparison.java
@@ -6,7 +6,7 @@ public class PoseComparison {
     
     int numPoses;
     PoseComparison(){
-        this.numPoses = 4;
+        this.numPoses = 3;
     }
 
     /**
@@ -25,26 +25,44 @@ public class PoseComparison {
         int i, j, classifiedPoseIndex=0, totalError, minError=10000;
         angleErrors = new float[this.numPoses][10];
         referenceAngles = getReferenceeAngles();
-        for(i=0; i<referenceAngles.length; i++){
+//        for(i=0; i<referenceAngles.length; i++){
+//            totalError = 0;
+//            for(j=0; j<10; j++){
+//                difference = 1 - (Math.abs(angleList[j] - referenceAngles[i][j])/referenceAngles[i][j]);
+//                error = (float) (100/(1 +Math.pow(Math.E,((-Math.E)*difference+(Math.pow(Math.E,2))))));
+//                angleErrors[i][j] = error;
+//                totalError += error;
+//            }
+//            if((totalError < minError) | (minError == 0)){
+//                minError = totalError;
+//                classifiedPoseIndex = i;
+//            }
+//        }
+        for (i = 0; i < referenceAngles.length; i++) {
             totalError = 0;
-            for(j=0; j<10; j++){
-                difference = 1 - (Math.abs(angleList[j] - referenceAngles[i][j])/referenceAngles[i][j]);
-                error = (float) (100/(1 +Math.pow(Math.E,((-Math.E)*difference+(Math.pow(Math.E,2))))));
+            for (j = 0; j < 10; j++) {
+                // Avoid division by zero
+                if (referenceAngles[i][j] == 0) {
+                    continue; // or handle appropriately
+                }
+                difference = Math.abs(angleList[j] - referenceAngles[i][j]) / referenceAngles[i][j];
+                error = (float)(difference * 100); // Error as a percentage
                 angleErrors[i][j] = error;
                 totalError += error;
             }
-            if((totalError < minError) | (minError == 0)){
+            if (totalError < minError) {
                 minError = totalError;
                 classifiedPoseIndex = i;
             }
         }
+
         return angleErrors[classifiedPoseIndex];
     }
     /**
     * INCOMPLETE FUNCTION. Function for getting the angles of defined poses.
     */
     private float[][] getReferenceeAngles(){
-        float referenceAngles[][] = {{90.0f, 90.0f, 180.0f, 180.0f, 180.0f, 180.0f, 180.0f, 180.0f, 135.0f, 135.0f}, {165.0f, 165.0f, 165.0f, 165.0f, 104.0f, 104.0f, 170.0f, 170.0f, 125.0f, 125.0f}, {26.0f, 26.0f, 47.0f, 47.0f, 116.5f, 116.5f, 116.5f, 116.5f, 125.5f, 125.5f}, {177.5f, 177.5f, 170.5f, 170.5f, 165.0f, 170.0f, 177.0f, 101.33f, 160.0f, 160.0f}};
+        float referenceAngles[][] = {{90.0f, 90.0f, 180.0f, 180.0f, 180.0f, 180.0f, 180.0f, 180.0f, 135.0f, 135.0f}, {165.0f, 165.0f, 165.0f, 165.0f, 104.0f, 104.0f, 170.0f, 170.0f, 125.0f, 125.0f}, {26.0f, 26.0f, 47.0f, 47.0f, 116.5f, 116.5f, 116.5f, 116.5f, 125.5f, 125.5f}};
 //        float[][] extractedAngles = extractAnglesFromImages("/path/to/golden_poses");
         return referenceAngles;
     }
